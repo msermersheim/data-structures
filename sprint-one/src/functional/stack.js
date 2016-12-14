0f2052e905a7dc8f11a0b2cc27666be4079b72ec
msermersheim <m.sermersheim@gmail.com>
2016-12-14 15:00:46 -0800

    All stack tests passing.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -7,9 +7,18 @@ var Stack = function() {
   // Implement the methods below
   someInstance.push = function(value) {
     size++;
+    storage[size] = value;
+    
   };
 
   someInstance.pop = function() {
+    var returnValue = storage[size];
+    delete storage[size];
+    size--;
+    if (size < 1) {
+      size = 0;
+    }
+    return returnValue;
   };
 
   someInstance.size = function() {
