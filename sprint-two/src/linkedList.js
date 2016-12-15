eca7ac397f8bc81c746cd325249fde9f08495ec7
msermersheim <m.sermersheim@gmail.com>
2016-12-15 10:49:18 -0800

    Pass all linkedList tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,29 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //list.tail = Node(value);
+    if (!list.tail) {
+      list.tail = Node(value);
+    } else {
+      list.head = list.tail;
+      list.tail = Node(value);
+    }
   };
 
   list.removeHead = function() {
+    list.head = list.tail;
+    //list.tail = null;
+    console.log(list.head);
+    return list.head.value;
   };
 
   list.contains = function(target) {
+    if (target === list.head.value) {
+      return true;
+    } else if (target === list.tail.value) {
+      return true;
+    }
+    return false;
   };
 
   return list;
