c46e20466867c6f8d45b4045be012fde94ca8b1a
msermersheim <m.sermersheim@gmail.com>
2016-12-14 20:02:21 -0800

    Tests for Prototypal Pattern passed

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,42 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  newQueue.sizeVar = 0;
+  newQueue.firstKey = 1;
+  newQueue.lastKey = 1;
+  newQueue.returnValue;
+
+  return newQueue;
 };
 
 var queueMethods = {};
 
 
+queueMethods.enqueue = function(value) {
+  this.sizeVar++;
+  this.storage[this.lastKey] = value;
+  this.lastKey++;
+};
+
+queueMethods.dequeue = function() {
+  //console.log(this.storage[this.firstKey]);
+  this.returnValue = this.storage[this.firstKey];
+  console.log(this.returnValue);
+  delete this.storage[this.firstKey];
+  this.sizeVar--;
+  this.firstKey++;
+  // console.log(this.firstKey);
+  if (this.sizeVar < 1) {
+    this.sizeVar = 0;
+    this.firstKey = 1;
+    this.lastKey = 1;
+  }
+  return this.returnValue;
+};
+
+queueMethods.size = function() {
+  return this.sizeVar;
+  
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.sizeVar = 0;
+
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  console.log(this.sizeVar);
+  return this.sizeVar;
+};
 
+stackMethods.push = function(value) {
+  this.sizeVar++;
+  this.storage[this.sizeVar] = value;
+};
+
+stackMethods.pop = function() {
+  var returnValue = this.storage[this.sizeVar];
+  delete this.storage[this.sizeVar];
+  this.sizeVar--;
+  if (this.sizeVar < 1) {
+    this.sizeVar = 0;
+  }
+  return returnValue;
+};
