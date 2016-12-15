0c0f15c733fd4f699f88bca5be6d3ddc3c09a4ef
msermersheim <m.sermersheim@gmail.com>
2016-12-14 20:40:49 -0800

    Pass all Pseudoclassical tests

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.sizeVar = 0;
+  this.firstKey = 1;
+  this.lastKey = 1;
+  
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.sizeVar++;
+  this.storage[this.lastKey] = value;
+  this.lastKey++;
+};
+
+Queue.prototype.dequeue = function(value) {
+  var returnValue = this.storage[this.firstKey];
+  console.log(this.returnValue);
+  delete this.storage[this.firstKey];
+  this.sizeVar--;
+  this.firstKey++;
+  if (this.sizeVar < 1) {
+    this.sizeVar = 0;
+    this.firstKey = 1;
+    this.lastKey = 1;
+  }
+  return returnValue;
+};
+
+Queue.prototype.size = function() {
+  return this.sizeVar;
+};
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.sizeVar = 0;
+  this.storage = {};
+
 };
 
+Stack.prototype.size = function() {
+  return this.sizeVar;
+};
 
+Stack.prototype.push = function(value) {
+  this.sizeVar++;
+  this.storage[this.sizeVar] = value;
+};
+
+Stack.prototype.pop = function() {
+  var returnValue = this.storage[this.sizeVar];
+  delete this.storage[this.sizeVar];
+  this.sizeVar--;
+  if (this.sizeVar < 1) {
+    this.sizeVar = 0;
+  }
+  return returnValue;
+};
