330f2a7d349aff9f05d189a28931e32e06c8986f
hanyen <me@hanyen.com>
2016-12-15 21:03:49 -0800

    Almost done with LinkList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,37 +2,83 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  //create a counter property
+  list.counter = 0;
+  list.keyTrackingArray = []; // use this to find node
 
   // 6 is passed to the function. value = 6
   list.addToTail = function(value) {
     // naive implementation
-    if (!list.tail) {
-      list.head = Node(value);
-      list.tail = Node(value);
-    } else {
-      //create a new object with key of 1
-      //list[1] = Node(value);
-      list.tail = Node(value);
-      list.head.next = 'tail';
-      // list.head = list.tail;
-      console.log(list);
-    }
+                                        /*if (!list.tail) {
+                                          list.head = Node(value);
+                                          list.tail = Node(value);
+                                        } else {*/
+      // for every value that's passed here
+      // create a node object and assign a DIFFERENT key each time
+        // use what to iterate the key? for loop? use counter? 
+        // Keep the counter value in the list object.
+
+                                          /*list.tail = Node(value);
+                                          list.head.next = 'tail';*/
+      
+      //create node for the value passed to this function
+      list[list.counter] = Node(value);
+      
+      // need to set the previous key to the new value
+      if (list.counter > 1) {
+        list[list.counter - 1].next = list.counter;
+      }
+      
+      //set head if (!list.head)
+      if (!list.head) {
+        list.head = Object.create(list[0]);
+      }
+      // use prototypal to look up head and tail value
+       
+      list.tail = Object.create(list[list.counter]);
+      
+      //push key to array to keep track of the linkedList key
+      list.keyTrackingArray.push(list.counter);
+
+      //increase counter by 1 at the end
+      list.counter++;
 
   };
 
   list.removeHead = function() {
-    list.head = list.tail;
-    return list.head.value;
+                      // naive implementation
+                              // list.head = list.tail;
+                              // return list.head.value;
+
+    // console.log(list.keyTrackingArray);
+    list.keyTrackingArray.shift();
+    // console.log(list.keyTrackingArray);
+
+    list.head = Object.create(list[list.keyTrackingArray[0]]);
   };
 
   list.contains = function(target) {
-    // naive implementation
-    if (target === list.head.value) {
-      return true;
-    } else if (target === list.tail.value) {
-      return true;
+                      // naive implementation
+                              // if (target === list.head.value) {
+                              //   return true;
+                              // } else if (target === list.tail.value) {
+                              //   return true;
+                              // }
+                              // return false;
+    
+    //convert list object to array
+      // var arr = Object.keys(list).map(function (key) { return list[key]; });
+      // console.log(arr);
+
+    //use list.keyTrackingArray to iterate through the keys
+    for (var i = 0; i < list.keyTrackingArray.length; i++) {
+      // need to think how to iterate the array
+      if (list[list.keyTrackingArray[i]].value === target ) {
+        return true;
+      }
     }
     return false;
+
   };
 
   return list;
